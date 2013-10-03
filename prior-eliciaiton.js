56437234628ba8a43f7e926e2c618a81f9bfae7d
feste <festethejester@gmail.com>
2013-10-02 17:18:48 -0700

    experiment skeleton

---
+++ prior-eliciaiton.js
@@ -0,0 +1,52 @@
+function caps(a) {return a.substring(0,1).toUpperCase() + a.substring(1,a.length);}
+function uniform(a, b) { return ( (Math.random()*(b-a))+a ); }
+function showSlide(id) { $(".slide").hide(); $("#"+id).show(); }
+function shuffle(v) { newarray = v.slice(0);for(var j, x, i = newarray.length; i; j = parseInt(Math.random() * i), x = newarray[--i], newarray[i] = newarray[j], newarray[j] = x);return newarray;} // non-destructive.
+
+$(document).ready(function() {
+  showSlide("consent");
+  $("#mustaccept").hide();
+});
+
+var experiment = {
+  data: {},
+  
+  instructions: function() {
+    if (turk.previewMode) {
+      $("#instructions #mustaccept").show();
+    } else {
+      showSlide("instructions");
+      $("#begin").click(function() { experiment.trial(); })
+    }
+  },
+  
+  trial: function(qNumber) {
+  },
+  
+  questionaire: function() {
+    //disable return key
+    $(document).keypress( function(event){
+     if (event.which == '13') {
+        event.preventDefault();
+      }
+    });
+    //progress bar complete
+    $('.bar').css('width', ( "100%"));
+    showSlide("questionaire");
+    $("#formsubmit").click(function() {
+      rawResponse = $("#questionaireform").serialize();
+      pieces = rawResponse.split("&");
+      var age = pieces[0].split("=")[1];
+      var lang = pieces[1].split("=")[1];
+      var comments = pieces[2].split("=")[1];
+      if (lang.length > 0) {
+        experiment.data["language"] = lang;
+        experiment.data["comments"] = comments;
+        experiment.data["age"] = age;
+        showSlide("finished");
+        setTimeout(function() { turk.submit(experiment.data) }, 1000);
+      }
+    });
+  }
+}
+  
